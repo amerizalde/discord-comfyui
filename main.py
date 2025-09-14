ef8173410c93e04d38120a68988fb97111739715
Andrew Merizalde <andrewmerizalde@hotmail.com>
2025-09-14 07:59:48 -0600

    comfyui prompt bot

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import discord
+import aiohttp
 
 from dotenv import load_dotenv
 import os
@@ -13,10 +14,21 @@ class Client(discord.Client):
     async def on_message(self, message):
         if message.author == self.user:
             return
+        
+        if message.channel.name == "comfyui":
 
-        if message.content.startswith('!hello'):
-            await message.channel.send('Hello!')
-            
+            if message.content.startswith('!hello'):
+                await message.channel.send('Hello!')
+                
+            if message.content.startswith('!prompt') or message.content.startswith('!P'):
+                prompt = message.content.strip('!prompt').strip('!P')
+                # send prompt to webhook via POST request AI
+                webhook_url = "http://192.168.0.244:5678/webhook-test/prompt"
+                
+                async with aiohttp.ClientSession() as session:
+                    await session.post(webhook_url, json={"prompt": prompt})
+                
+                
 intents = discord.Intents.default()
 intents.message_content = True
 
